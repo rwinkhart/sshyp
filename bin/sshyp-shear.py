7f005702492afa9df45aa0adb22d191b0eddb9b8
Cuan <idgr@tutanota.com>
2022-02-02 09:23:43 -0500

    Implemented multi-client deletion

--- bin/sshyp-shear.py
+++ bin/sshyp-shear.py
@@ -8,8 +8,23 @@ from os.path import expanduser
 
 # utility functions
 
-def delete(_file_path, _client_device_name):
-    remove(f"{expanduser('~/.password-pasture/')}{_file_path}")
+def delete(_file_path):
+    try:
+        remove(f"{expanduser('~/.password-pasture/')}{_file_path}.gpg")
+    except FileNotFoundError:
+        print('\nFile does not exist remotely.\n')
     for _device_name in listdir(expanduser('~/.config/sshyp/devices')):
-        open(f"{expanduser('~/.config/sshyp/deleted/')}{_file_path}.{_device_name}.del", 'w')
-    remove(f"{expanduser('~/.config/sshyp/deleted/')}{_file_path}.{_client_device_name}.del")
+        open(f"{expanduser('~/.config/sshyp/deleted/')}{_file_path.replace('/', '@')}.{_device_name}.del", 'w')
+
+
+def check(_client_device_name):
+    open(expanduser('~/.config/sshyp/deletion_database'), 'w').write('')
+    for _file in listdir(expanduser('~/.config/sshyp/deleted')):
+        _file_path = _file.replace('.gpg', '').replace('@', '/').split('.')[0]
+        _device = _file.replace('.gpg', '').split('.')[1]
+        if _device == _client_device_name:
+            try:
+                remove(f"{expanduser('~/.config/sshyp/deleted/')}{_file}")
+            except FileNotFoundError:
+                pass
+            open(expanduser('~/.config/sshyp/deletion_database'), 'a').write(_file_path + '.gpg\n')
