0314bf36af8526dfa001e5174e1e46954b99eddf
Cuan <idgr@tutanota.com>
2022-02-15 23:41:23 -0500

    Added folder deletion

--- bin/sshyp-shear.py
+++ bin/sshyp-shear.py
@@ -4,15 +4,22 @@
 
 from os import remove, listdir
 from os.path import expanduser
+from shutil import rmtree
 
 
 # utility functions
 
 def delete(_file_path):
-    try:
-        remove(f"{expanduser('~/.password-pasture/')}{_file_path}.gpg")
-    except FileNotFoundError:
-        print('\nFile does not exist remotely.\n')
+    if _file_path.endswith('/'):
+        try:
+            rmtree(f"{expanduser('~/.password-pasture/')}{_file_path}")
+        except FileNotFoundError:
+            print('\nFolder does not exist remotely.')
+    else:
+        try:
+            remove(f"{expanduser('~/.password-pasture/')}{_file_path}.gpg")
+        except FileNotFoundError:
+            print('\nFile does not exist remotely.\n')
     for _device_name in listdir(expanduser('~/.config/sshyp/devices')):
         open(f"{expanduser('~/.config/sshyp/deleted/')}{_file_path.replace('/', '@')}.{_device_name}.del", 'w')
 
