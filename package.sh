1e0aa8aa391200ece032847141f4d7766a3765b3
Randall Winkhart <idgr@tutanota.com>
2023-03-27 23:08:55 -0400

    Made source variables explicitly local
    Former-commit-id: 0f7810121a1d7a905b10b208d0f31df2480207e8
    Former-commit-id: 820c9bfcc79dadb363a1c62e615854a294b64a3a

--- package.sh
+++ package.sh
@@ -28,7 +28,7 @@ _create_generic() {
 } &&
 
 _create_pkgbuild() {
-    source='https://github.com/rwinkhart/sshyp/releases/download/v$pkgver/sshyp-$pkgver.tar.xz'
+    local source='https://github.com/rwinkhart/sshyp/releases/download/v$pkgver/sshyp-$pkgver.tar.xz'
     printf '\ngenerating PKGBUILD...\n'
     printf "# Maintainer: Randall Winkhart <idgr at tutanota dot com>
 pkgname=sshyp
@@ -51,7 +51,7 @@ package() {
 } &&
 
 _create_apkbuild() {
-    source='https://github.com/rwinkhart/sshyp/releases/download/v$pkgver/sshyp-$pkgver.tar.xz'
+    local source='https://github.com/rwinkhart/sshyp/releases/download/v$pkgver/sshyp-$pkgver.tar.xz'
     printf '\ngenerating APKBUILD...\n'
     printf "# Maintainer: Randall Winkhart <idgr@tutanota.com>
 pkgname=sshyp
