d40d1daab5da937ebd3112da3b118f2d1f6c36c1
Randall Winkhart <idgr@tutanota.com>
2023-07-08 20:25:59 -0400

    Fixed Haiku using the wrong Python version, made building on Haiku easier
    Former-commit-id: f5d19c55405790060136af4f2a012987ce88d5f2
    Former-commit-id: 69aef95294b038c405d95ad3aa37c0a1991a9f8a

--- port-jobs/SHEBANG.sh
+++ port-jobs/SHEBANG.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-if [ "$1" = 'HAIKU' ]; then
+if [ "$1" = 'PREP' ]; then
+    sed -i '1 s/.*/#!\/bin\/env\ python3.10/' ./*.py
+else
     sed -i '1 s/.*/#!\/bin\/env\ python3.10/' ./working/sshyp.py
 fi
