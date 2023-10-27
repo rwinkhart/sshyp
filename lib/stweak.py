dbae72b926270b4b682050ce678860d99b66f0cf
Randall Winkhart <idgr@tutanota.com>
2023-10-26 20:30:49 -0400

    More clearly present extension information in the install screen
    Former-commit-id: d75086ed21f45966b6c2b0074aea6e288d1ce650
    Former-commit-id: fcf66ffac17cfc97868b0dc6dd4f4c4077f90fd3

--- lib/stweak.py
+++ lib/stweak.py
@@ -371,8 +371,10 @@ def extension_downloader():
     if _choice == len(_extensions)-1:
         return False
     _selected = _extensions[_choice]
-    _choice = curses_radio(('no', 'yes'), 'description: ' + _pointer.get(_selected, 'desc') + '\n\nusage: ' +
-                           _pointer.get(_selected, 'usage').replace('<br>', '\n') + '\n\ninstall ' + _selected + '?')
+    _divider = (get_terminal_size()[0]//2)*'-'
+    _choice = curses_radio(('no', 'yes'), '/description/\n' + _divider + '\n\n' + _pointer.get(_selected, 'desc') +
+                           '\n\n/usage/\n' + _divider + '\n\n' + _pointer.get(_selected, 'usage').replace('<br>', '\n')
+                           + '\n\n' + _divider + '\n\ninstall ' + _selected + '?')
     # if installing the extension...
     if _choice == 1:
         # download extension files to temporary directory
